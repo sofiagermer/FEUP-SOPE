ec1910d3343ccf3d326cfe54353e88f502809cdb
Miguel Lopes <lopesazevedom@gmail.com>
2021-04-24 18:07:21 +0100

    errors everywhere :)

--- MP2/main.c
+++ MP2/main.c
@@ -1,7 +1,18 @@
 
-#include "parser.h"
-#include <pthread.h>
+
 #include <fcntl.h>
+#include <stdbool.h>
+#include <time.h>
+#include <pthread.h>
+
+
+#include "parser.h"
+
+info_t info;
+
+void regist(int i, pid_t pid, pthread_t tid, int res,char* oper){
+    printf("%ld ; %d ; %d ; %ld ; %d ; %s" ,time(NULL),i,pid,tid,res,oper);
+}
 
 typedef struct{
     int i;
@@ -11,13 +22,27 @@ typedef struct{
     int res;
 } msg;
 
-void writeToPublicFifo(char* name,msg* message){
+int createFifo(){
+    if(mkfifo(info.fifoname, 0666) == -1){
+        if(errno != EEXIST){
+            perror("Creating FIFO: ");
+            return 1;
+        }
+        else{
+           return 0;
+        }
+    }
+    return 0;
+}
+
+int writeToPublicFifo(msg* message){
     int np;
-    //if(mkfifo(name,0666)<0)
-        //perror("Error creating fifo");
-    while ((np = open (name,O_WRONLY )) < 0);
+    if(createFifo() != 0) return 1;
+    while ((np = open (info.fifoname,O_WRONLY )) < 0);
     write(np,message,sizeof(*message));
+    regist(message->i,message->pid,message->tid,message->res,"IWANT");
     close(np);
+    return 0;
 }
 
 void randomWait(){
@@ -27,37 +52,63 @@ void randomWait(){
 }
 
 void *threadHandler(void *i){
-    msg *message;
-    message->i=(int)i;
-    message->t=(rand()%10)+1;
-    message->pid=getpid();
-    message->tid=pthread_self();
-    message->res=-1;
+    msg message; 
+    message.i =*(int *) i;
+    message.t=(rand()%10)+1;
+    message.pid=getpid();
+    message.tid=pthread_self();
+    message.res=-1;
+
+    int privateFifo;
 
     //Creates private fifo name in format pid.tid
     char privateFifoName[200];
-    snprintf(privateFifoName,200,"/tmp/%d.%ld",message->pid,message->tid);
+    snprintf(privateFifoName,200,"/tmp/%d.%ld",message.pid,message.tid);
+    if(mkfifo(privateFifoName, 0666) == -1){
+        if(errno != EEXIST){
+            perror("Creating FIFO: ");
+            //return 1;
+        }
+        
+    }
+    while ((privateFifo = open (privateFifoName, O_RDONLY)) < 0);
+    read(privateFifo,&message,sizeof(message));
 
+    if(message.res==-1){
+        regist(message.i,message.pid,message.tid,message.res,"CLOSD");
+    }
+    else if(message.res!=-1) regist(message.i,message.pid,message.tid,message.res,"GOTRS");
+    else regist(message.i,message.pid,message.tid,message.res,"FAILD");
 
+    close(privateFifo);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char const * argv[])
 {
-    info_t info;  //maybe there is no need for a struct and we can just leave fifoname and nsecs separated
+    int msec = 0, trigger = 3000; /* 10ms */
+    clock_t before = clock();
     int i=1;
-    int *threadIds=malloc(sizeof(int)*i);
+    pthread_t *ids=(pthread_t*)malloc(1*sizeof(pthread_t));;
     parse(&info, argc, argv);
     printf("name %s\n", info.fifoname);  //just debug
     printf("nsecs %i\n", info.nsecs);
     //nsecs is validated, fifoname isn't validated yet (pipe might not exist)
-    while(1){
+    while(msec < trigger){
+        clock_t difference = clock() - before;
+        msec = difference * 1000 / CLOCKS_PER_SEC;
         
-        if(pthread_create(threadIds[i-1],NULL,threadHandler,i)){
+        if(pthread_create(&ids[i-1],NULL,threadHandler,&i)){
 
         }
-        
+
         i++;
-        randomWait();
+        ids = (pthread_t*)realloc(ids, i*sizeof(pthread_t));
+        //randomWait();
+    }
+    printf("%d\n",getpid());
+    for(int j=0; j<i; j++) {
+        pthread_join(ids[j], NULL);
     }
     return 0;
 }
