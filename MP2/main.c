0eab04c9611e0046c687c2e6f926749807ec34b2
marhcouto <up201906086@fe.up.pt>
2021-04-24 18:56:34 +0100

    Missing mutexes/semaphores and organization

--- MP2/main.c
+++ MP2/main.c
@@ -1,11 +1,7 @@
-
-
 #include <fcntl.h>
 #include <stdbool.h>
 #include <time.h>
 #include <pthread.h>
-
-
 #include "parser.h"
 
 info_t info;
@@ -23,20 +19,16 @@ typedef struct{
 } msg;
 
 int createFifo(){
-    if(mkfifo(info.fifoname, 0666) == -1){
-        if(errno != EEXIST){
-            perror("Creating FIFO: ");
-            return 1;
-        }
-        else{
-           return 0;
-        }
+    if(mkfifo(info.fifoname, 0666) == -1 && errno != EEXIST){
+        perror("Failed to create FIFO");
+        return 1;
     }
     return 0;
 }
 
-int writeToPublicFifo(msg* message){
+int writeToPublicFifo(msg* message) {
     int np;
+
     if(createFifo() != 0) return 1;
     while ((np = open (info.fifoname,O_WRONLY )) < 0);
     write(np,message,sizeof(*message));
@@ -47,7 +39,7 @@ int writeToPublicFifo(msg* message){
 
 void randomWait(){
     //dorme qlq coisa entre 0 e 100 milisegundos
-    int time=(rand()%100000)+1000;
+    int time = (rand() % 100000);
     usleep(time);
 }
 
@@ -64,19 +56,19 @@ void *threadHandler(void *i){
     //Creates private fifo name in format pid.tid
     char privateFifoName[200];
     snprintf(privateFifoName,200,"/tmp/%d.%ld",message.pid,message.tid);
+
     if(mkfifo(privateFifoName, 0666) == -1){
-        if(errno != EEXIST){
-            perror("Creating FIFO: ");
-            //return 1;
-        }
-        
+        perror("Failed to create FIFO");
+        exit(1);
     }
+
     while ((privateFifo = open (privateFifoName, O_RDONLY)) < 0);
     read(privateFifo,&message,sizeof(message));
 
     if(message.res==-1){
         regist(message.i,message.pid,message.tid,message.res,"CLOSD");
     }
+
     else if(message.res!=-1) regist(message.i,message.pid,message.tid,message.res,"GOTRS");
     else regist(message.i,message.pid,message.tid,message.res,"FAILD");
 
@@ -84,31 +76,34 @@ void *threadHandler(void *i){
     pthread_exit(NULL);
 }
 
-int main(int argc, char const * argv[])
-{
-    int msec = 0, trigger = 3000; /* 10ms */
-    clock_t before = clock();
-    int i=1;
-    pthread_t *ids=(pthread_t*)malloc(1*sizeof(pthread_t));;
+int main(int argc, char const * argv[]) {
+    time_t start,end;
+    time(&start);
+    double sec,trigger = 3;
+
+    int i = 1;
+    pthread_t *ids = (pthread_t*)malloc(1 * sizeof(pthread_t));
+    
     parse(&info, argc, argv);
-    printf("name %s\n", info.fifoname);  //just debug
-    printf("nsecs %i\n", info.nsecs);
-    //nsecs is validated, fifoname isn't validated yet (pipe might not exist)
-    while(msec < trigger){
-        clock_t difference = clock() - before;
-        msec = difference * 1000 / CLOCKS_PER_SEC;
+
+
+    while(sec < trigger){
+        time(&end);sec = end-start;
         
         if(pthread_create(&ids[i-1],NULL,threadHandler,&i)){
-
+            fprintf(stderr, "Failed to create thread: %s\n", strerror(errno));
+            return 1;
         }
 
         i++;
         ids = (pthread_t*)realloc(ids, i*sizeof(pthread_t));
-        //randomWait();
+        printf("i:%d mils:%f\n", i, sec);
+        randomWait();
     }
-    printf("%d\n",getpid());
+
     for(int j=0; j<i; j++) {
         pthread_join(ids[j], NULL);
     }
+
     return 0;
 }
