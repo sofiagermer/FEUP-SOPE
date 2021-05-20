a187b9580f1dad922d128ad567ce3b285707483a
marhcouto <marhcouto1101@gmail.com>
2021-05-20 15:28:21 +0100

    K

--- MP2/server/parser.c
+++ MP2/server/parser.c
@@ -25,16 +25,14 @@ void parse(info_t * info, int argc, char * argv[]) {
     info->fifoname = NULL;
     for (unsigned i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-t") == 0) { //Time
-            if ( i == argc - 1 || (i != argc - 1 && !is_positive_integer(argv[i+1])) ) 
-            {
+            if ( i == argc - 1 || (i != argc - 1 && !is_positive_integer(argv[i+1]))) {
                 printf("s: error: missing time value after -t\n"); 
                 exit(1);
             }
             info->nsecs = atoi(argv[i+1]);
             i++;
-        }else if(strcmp(argv[i], "-l") == 0){ //buffer size
-            if ( i == argc - 1 || (i != argc - 1 && !is_positive_integer(argv[i+1])) ) 
-            {
+        } else if(strcmp(argv[i], "-l") == 0) { //buffer size
+            if ( i == argc - 1 || (i != argc - 1 && !is_positive_integer(argv[i+1]))) {
                 printf("s: error: missing time value after -l\n"); 
                 exit(1);
             }
@@ -42,20 +40,19 @@ void parse(info_t * info, int argc, char * argv[]) {
             i++;
         }
         else { //Fifopath
-            if(info->fifoname != NULL){
+            if(info->fifoname != NULL) {
                 printf("s: error: arguments aren't correct\n"); 
                 exit(1);
             }
             info->fifoname = argv[i];
         }
     }
-    if(info->nsecs == -1){
+    if (info->nsecs == -1) {
         printf("s: error: missing option -t \n"); 
         exit(1);
     }
-    if(info->fifoname == NULL){
+    if (info->fifoname == NULL) {
         printf("s: error: missing fifoname \n"); 
         exit(1);
     }
-
 }
--- MP2/server/threads.c
+++ MP2/server/threads.c
@@ -4,8 +4,8 @@ extern info_t info;
 extern int errno;
 
 int publicFifoDesc; 
-sem_t semP, semC;
-pthread_mutex_t indexMutex = PTHREAD_MUTEX_INITIALIZER;
+sem_t semP, semC; // Semaphores for consumer and producer threads to access the buffer
+pthread_mutex_t indexMutex = PTHREAD_MUTEX_INITIALIZER; // Mutex for the index of the buffer
 msg** buffer;
 int bufferIndex;
 bool timeout;
@@ -13,52 +13,68 @@ bool timeout;
 void* consumerHandler(void* a) {
     msg* message;
 
-    // Semaphore to wait when buffer is empty
-    int index=0;
-    while(!timeout){    
+    int index = 0;
+    while (true) {    
+        printf("STUCK\n");
+
+        // Conditions to end the loop
+        int result;
+        if (sem_getvalue(&semC, &result) != 0) {
+            fprintf(stderr, "Server: Error reading semaphore value in %s: %s\n", __func__, strerror(errno));
+            exit(1);
+        }
+        if (result == 0 && timeout) break;
+
+        // Semaphore to wait when buffer is empty
         sem_wait(&semC);
-        printf("here\n");
-        message=buffer[index];
+        message = buffer[index];
         index++;
-        index=index%info.buffersize;
+        index = index % info.buffersize;
         sem_post(&semP);
+
         char privateFifoName[200];
-        snprintf(privateFifoName, sizeof(privateFifoName), "/tmp/%d.%ld", message->pid, message->tid);
-        
         int privateFifoDesc;
 
-        while((privateFifoDesc = open(privateFifoName, O_WRONLY)) < 0) {
-            fprintf(stderr, "Failed to open private FIFO: %s\n", strerror(errno));
-            exit(1);
+        snprintf(privateFifoName, sizeof(privateFifoName), "/tmp/%d.%ld", message->pid, message->tid);
+
+        if((privateFifoDesc = open(privateFifoName, O_WRONLY)) < 0) {
+            regist(message->i,message->t,getpid(),pthread_self(),message->res,"FAILD");
+            fprintf(stderr, "Server: Failed to open private FIFO in %s: %s\n", __func__, strerror(errno));
+            printf("CONSUMER GONNA DIE NOW\n");
+            pthread_exit(NULL);
         }
-        message->pid=getpid();
-        message->tid=pthread_self();
+
+        printf("GOT THERE\n");
         
         if (write(privateFifoDesc,message,sizeof(msg)) < 0) {
-            fprintf(stderr, "Failed to write to private fifo: %s\n", strerror(errno));
+            fprintf(stderr, "Server: Failed to write to private fifo in %s: %s\n", __func__, strerror(errno));
             exit(1);
         }
-        regist(message->i,message->t,message->pid,message->tid,message->res,"TSKDN");
+        regist(message->i,message->t,getpid(),pthread_self(),message->res,"TSKDN");
     }
+    printf("CONSUMER GONNA DIE NOW\n");
     pthread_exit(NULL);
 }
 
 void* producerHandler(void* a) {
-    msg* message=(msg*)a;
+    msg* message = (msg*) a;
+
+    // Perform task
+    message->res = task(message->t);
+    regist(message->i,message->t,getpid(),pthread_self(),message->res,"TSKEX");
+
     // Semaphore to wait when buffer is full
     sem_wait(&semP);
-    message->res=task(message->t);
-    regist(message->i,message->t,message->pid,message->tid,message->res,"TSKEX");
 
     // Mutex to access buffer index variable
     pthread_mutex_lock(&indexMutex);
-    int localIndex=bufferIndex;
+    int localIndex = bufferIndex;
     bufferIndex++;
-    bufferIndex=bufferIndex%info.buffersize;
+    bufferIndex = bufferIndex % info.buffersize;
     pthread_mutex_unlock(&indexMutex);
     
     // Write message to buffer
-    buffer[localIndex]=message;
+    buffer[localIndex] = message;
     sem_post(&semC);
     pthread_exit(NULL);
 }
@@ -66,52 +82,69 @@ void* producerHandler(void* a) {
 
 void createThreads() {
 
+    printf("I am here\n");
+
+    timeout = false;
+    msg* message;
+    bufferIndex = 0;
+
     // Allocation of the buffer
     buffer = (msg**) malloc(info.buffersize * sizeof(msg));
     bufferIndex = 0;
 
     // Time, semaphores and mutex
-    time_t start, now;
+    time_t start;
     time(&start);
     sem_init(&semP, 0, info.buffersize);
     sem_init(&semC, 0, 0);
 
+    // Try opening public fifo
+    while ((publicFifoDesc = open(info.fifoname, O_RDONLY | O_NONBLOCK)) < 0 && (time(NULL) - start < info.nsecs)) {
+        printf("TRYING TO OPEN\n");
+    }
+    if (publicFifoDesc == -1) { // Timeout
+        fprintf(stderr, "Server: Error opening public fifo in %s:timeout reached\n", __func__);
+        sem_destroy(&semC);
+        sem_destroy(&semP);
+        free(buffer);
+        exit(1);
+    }
+    printf("FILE DESCRIPTOR:%d\n", publicFifoDesc);
+
     // Consumer thread
     pthread_t id;
     if (pthread_create(&id, NULL, consumerHandler, NULL) != 0) {
-        fprintf(stderr, "Error creating thread: %s\n", strerror(errno));
+        fprintf(stderr, "Server: Error creating thread in %s: %s\n", __func__, strerror(errno));
         exit(1);
     }
     push(id);
 
-    
-    while ((publicFifoDesc = open(info.fifoname, O_RDONLY)) < 0); 
-    if (publicFifoDesc < 0) {
-        fprintf(stderr, "Error opening public fifo: %s - (timeout reached)\n", strerror(errno));
-        exit(1);
-    }
-    time(&now);
-
-    msg* message;
     // Producer threads (read message and create thread)
-    timeout=false;
-    while((int) (now - start) < info.nsecs) {
+    while((int) (time(NULL) - start) < info.nsecs) {
         
         message = (msg*) malloc(sizeof(msg));
+        int result;
         
-        while(read(publicFifoDesc, message, sizeof(msg)) == 0) {
-            //fprintf(stderr, "Error reading from public fifo: %s\n", strerror(errno));
-            //exit(1);
+        while((result = read(publicFifoDesc, message, sizeof(msg))) < 0 && (int) time(NULL) - start < info.nsecs);
+        if (result == -1) {
+            fprintf(stderr, "Server: Error reading from public fifo in %s:%s\n", __func__, strerror(errno));
+            exit(1);
+        } else if (result == 0) { // Timeout reached
+            free(message);
+            break;
         }
+
+        regist(message->i, message->t, getpid(), pthread_self(), message->res, "RECVD");
+
         if (pthread_create(&id, NULL, producerHandler, message)) {
-            fprintf(stderr, "Error creating thread:%s\n", strerror(errno));
+            fprintf(stderr, "Server: Error creating thread in %s:%s\n", __func__, strerror(errno));
             exit(1);
         }
         push(id);
-        time(&now);
     }
-    timeout=true;
-    if (read(publicFifoDesc, message, sizeof(msg)) == -1) {
+
+    timeout = true;
+    /* if (read(publicFifoDesc, message, sizeof(msg)) == -1) {
         fprintf(stderr, "Server: Error in %s:%s\n", __func__, strerror(errno));
         exit(1);
     }
@@ -132,11 +165,13 @@ void createThreads() {
         exit(1);
     }
 
-    regist(message->i,message->t,message->pid,message->tid,message->res,"2LATE");
+    regist(message->i,message->t,message->pid,message->tid,message->res,"2LATE"); */
+
     // Wait for all threads to finish
     while(!isEmpty()){
+        printf("One more\n");
         if (pthread_join(pop(), NULL) != 0) {
-            fprintf(stderr, "Error in pthread_join: %s\n", strerror(errno));
+            fprintf(stderr, "Server: Error in pthread_join in %s:%s\n", __func__, strerror(errno));
         }
     }
 
